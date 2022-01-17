6f981bafe16b2019fdd162f243309b0f498a620e
Joel <jpeddhi@contractor.lb.com>
2022-01-17 22:57:05 +0530

    Using Closures as parameters when they accept parameters

--- Contents.swift
+++ Contents.swift
@@ -708,6 +708,30 @@ crawl { (place: String) in
     print("I'm going to \(place) in my car")
 }
 
+// USING CLOSURES AS PARAMETERS WHEN THEY RETURN VALUES
+
+func travel(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I Arrived")
+}
+
+travel { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+// NEXT TOPICS TO COVER
+
+// STRUCTS
+
+//CLASSES
+
+//PROTOCOLS
+
+//OPTIONALS
+
+
+
 
 
 
